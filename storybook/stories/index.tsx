54b59ee903154cd21ab12a19aaea701cfe76390a
Damien Assany <damien.assany@live.fr>
2019-05-11 13:25:43 +0200

    chore(storybook): setup environment + switch in index to show App or Storybook

---
+++ storybook/stories/index.tsx
@@ -0,0 +1,34 @@
+import React, { SFC } from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+const bindStyle = (styles: any, props: any) => StyleSheet.flatten({
+  ...styles,
+  ...props
+});
+
+const style = {
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
+
+const CenteredView: SFC<{ backgroundColor?: string }> = ({ children, backgroundColor = "#F5FCFF" }) => (
+  <View style={bindStyle(style.centeredView, { backgroundColor })}>
+    {children}
+  </View>
+);
+
+storiesOf('CenteredView', module)
+  .add('default view', () => (
+    <CenteredView>
+      <Text>Hello Storybook</Text>
+    </CenteredView>
+  ))
+  .add('with red background', () => (
+    <CenteredView backgroundColor="red">
+      <Text>Hello Storybook</Text>
+    </CenteredView>
+  ));
