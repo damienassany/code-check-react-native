f966d70f722f7234e7697835fca5497f3541923e
Damien Assany <damien.assany@live.fr>
2019-05-11 18:12:45 +0200

    feat(theme): add theme helper

---
+++ src/shared/helpers/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+    fs: {
+        header: 21,
+        label: 18,
+        headerIcon: 25
+    },
+    colors: {
+        blue: '#0084b2',
+        lightBlue: '#019ad0',
+        red: '#d32e30',
+        white: '#fff',
+        black: "#222"
+    }
+}
