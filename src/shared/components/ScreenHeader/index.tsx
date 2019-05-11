110291f11cabb33d4850a8edcf3f62eede3d87de
Damien Assany <damien.assany@live.fr>
2019-05-11 18:21:35 +0200

    feat(ScreenHeader): add component + stories

---
+++ src/shared/components/ScreenHeader/index.tsx
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Label } from '../Label';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { theme } from '../../helpers/theme';
+
+interface Props {
+    icon: string;
+    label: string;
+    color?: string;
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "center",
+        width: '100%',
+        height: 70
+    },
+    icon: {
+        position: 'absolute',
+        left: 10
+    },
+});
+
+class ScreenHeader extends PureComponent<Props> {
+    public render() {
+        const { icon, label, color = theme.colors.black } = this.props;
+
+        return (
+            <View style={styles.wrapper}>
+                <Icon style={styles.icon} name={icon} size={theme.fs.headerIcon} color={color} />
+                <Label color={color} size={theme.fs.header} text={label}></Label>
+            </View>
+        );
+    }
+}
+
+export default ScreenHeader;
--- storybook/storybook.ts
+++ storybook/storybook.ts
@@ -4,6 +4,7 @@ import { getStorybookUI, configure } from '@storybook/react-native';
 // import stories
 configure(() => {
   require('./stories/Label');
+  require('./stories/ScreenHeader');
 }, module);
 
 const StorybookUI = getStorybookUI({ port: 7007, onDeviceUI: true });
